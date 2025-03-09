d982ced85baeb51190dcd2836d13da31c8893921
Terra Manning <terradmanning@gmail.com>
2025-03-08 17:15:35 -0700

    Initial commit of frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-10">
+      <h1 className="text-xl font-bold">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
+
